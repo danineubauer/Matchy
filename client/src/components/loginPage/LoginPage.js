d8561239601154a78eada56eb83a3a9ce65636e6
danineubauer <danineubauerr@gmail.com>
2021-01-02 17:22:06 -0800

    adds other pages

--- client/src/components/loginPage/LoginPage.js
+++ client/src/components/loginPage/LoginPage.js
@@ -7,9 +7,12 @@ export const LoginPage = () => {
     <div>
       <div className="login-main-container">
         <div className="login-popup-container">
-          <h2 className="login-header">Log in</h2>
+          <h2 className="login-headers login-header">Log in / Sign up</h2>
+          {/* <h2 className="login-headers signup-header">Sign up</h2> */}
           <input></input>
-          <button className="log-in-button">Log in</button>
+          <button className="login-button">Log in</button>
+          <button className="login-button signup-button">Sign up</button>
+          <h4>Forgot username / password?</h4>
         </div>
       </div>
     </div>
