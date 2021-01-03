cd58062ff5a47e45bff9d80d893a6bd132d21ae4
danineubauer <danineubauerr@gmail.com>
2021-01-02 17:24:01 -0800

    minor edit

---
+++ client/src/components/profilePage/ProfilePage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../shared/Theme.css";
+import "./ProfilePage.css";
+
+export const ProfilePage = () => {
+  return (
+    <div>
+      <div className="login-main-container">
+        <div className="login-popup-container">
+          <h2 className="login-headers login-header">Log in / Sign up</h2>
+          {/* <h2 className="login-headers signup-header">Sign up</h2> */}
+          <input></input>
+          <button className="login-button">Log in</button>
+          <button className="login-button signup-button">Sign up</button>
+          <h4>Forgot username / password?</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
