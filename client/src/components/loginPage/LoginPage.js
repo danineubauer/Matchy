dd2d8b680388782d5219e41e5680024a8d372d46
danineubauer <danineubauerr@gmail.com>
2021-01-02 20:21:32 -0800

    adds input to login page

--- client/src/components/loginPage/LoginPage.js
+++ client/src/components/loginPage/LoginPage.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "../shared/Theme.css";
 import "./LoginPage.css";
 
 export const LoginPage = () => {
+  const [displayLoginPage, setDisplayLoginPage] = useState();
+  const [displaySignupPage, setDisplaySignupPage] = useState();
+
   return (
     <div>
       <div className="login-main-container">
         <div className="login-popup-container">
-          <h2 className="login-headers login-header">Log in / Sign up</h2>
-          {/* <h2 className="login-headers signup-header">Sign up</h2> */}
-          <input></input>
-          <button className="login-button">Log in</button>
+          <h2
+            className="login-headers login-header"
+            onClick={() => setDisplayLoginPage(!displayLoginPage)}
+          >
+            Log in
+          </h2>
+
+          <h2
+            className="login-headers signup-header"
+            onClick={() => setDisplaySignupPage(!displaySignupPage)}
+          >
+            Sign up
+          </h2>
+
+          {displaySignupPage ? (
+            <h2>
+              <form>
+                <label>
+                  Name:
+                  <input type="text" name="name" />
+                </label>
+                <label>
+                  Last Name:
+                  <input type="text" name="last-name" />
+                </label>
+                <label>
+                  Email:
+                  <input type="text" name="email" />
+                </label>
+                <label>
+                  Password:
+                  <input type="text" name="password" />
+                </label>
+                <label>
+                  Repeat Password:
+                  <input type="text" name="repeat-password" />
+                </label>
+                <input className="login-button" type="submit" value="submit" />
+              </form>
+            </h2>
+          ) : (
+            <p></p>
+          )}
+          {displayLoginPage ? <h2></h2> : <p></p>}
+
           <button className="login-button signup-button">Sign up</button>
+          <button className="login-button">Log in</button>
           <h4>Forgot username / password?</h4>
         </div>
       </div>
