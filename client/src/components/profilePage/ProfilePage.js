b615ea44d848ddce60cf66dea8f7ac49925f04ed
danineubauer <danineubauerr@gmail.com>
2021-01-08 15:55:53 -0800

    edits profile page

--- client/src/components/profilePage/ProfilePage.js
+++ client/src/components/profilePage/ProfilePage.js
@@ -1,9 +1,12 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 import "./ProfilePage.css";
 
 export const ProfilePage = () => {
+  const [value, setValue] = useState();
+
   let fileSelectedHandler = (event) => {
-    console.log(event.target.files[0]);
+    setValue(URL.createObjectURL(event.target.files[0]));
   };
 
   return (
@@ -13,24 +16,52 @@ export const ProfilePage = () => {
           <h2 className="profilepage-header">Hi [name]! Welcome to Matchy!</h2>
           <p>please edit your profile to start finding your match!</p>
 
-          <div className="profilepage-profilephoto-container">
-            <input
-              type="file"
-              id="select-photo"
-              accept="image/*"
-              name="select-photo"
-              onChange={fileSelectedHandler}
-              hidden
-            />
-            <label
-              className="btn-select-photo"
-              for="select-photo"
-              role="button"
-            >
-              select photo
-            </label>
-            <div className="photo-preview-box"></div>
+          <img className="profile-photo-container" src={value} />
+          <input
+            type="file"
+            id="select-photo"
+            accept="image/*"
+            name="select-photo"
+            onChange={fileSelectedHandler}
+            hidden
+          />
+          <label className="btn-select-photo" for="select-photo" role="button">
+            select photo
+          </label>
+          <p>I am:</p>
+          <div className="gender-container">
+            <input type="checkbox" id="male" name="male" />
+            <label for="male">Male</label>
+            <input type="checkbox" id="female" name="female" />
+            <label for="female">Female</label>
+            <input type="checkbox" id="other" name="other" />
+            <label for="other">other</label>
           </div>
+          <p>Looking to meet:</p>
+          <div className="gender-container">
+            <input type="checkbox" id="male" name="male" />
+            <label for="male">Male</label>
+            <input type="checkbox" id="female" name="female" />
+            <label for="female">Female</label>
+            <input type="checkbox" id="other" name="other" />
+            <label for="other">other</label>
+          </div>
+          <div className="birthday">
+            <label for="birthdate">My birthday is on:</label>
+            <input type="date"></input>
+          </div>
+          <div className="favorite-color">
+            <input label="favorite color" type="color"></input>
+          </div>
+          <div>
+            <label for="hobbies">What's your favorite hobby?</label>
+            <input type="text" id="hobbies"></input>
+          </div>
+          <div>
+            <label for="hobbies">What's your favorite food?</label>
+            <input type="text" id="hobbies"></input>
+          </div>
+          <button className="profilepage-button">Finalize profile</button>
         </div>
       </div>
     </div>
