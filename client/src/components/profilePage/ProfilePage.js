2a1e1540ccfbdc3e93415cfa4afe6908b9193ba4
danineubauer <danineubauerr@gmail.com>
2021-01-10 10:01:03 -0800

    edit

--- client/src/components/profilePage/ProfilePage.js
+++ client/src/components/profilePage/ProfilePage.js
@@ -1,40 +1,19 @@
-// import React from "react";
-import React, { useState } from "react";
+import React from "react";
+import { SelectPhoto } from "./SelectPhoto.js";
 import "./ProfilePage.css";
 
 export const ProfilePage = () => {
-  const [value, setValue] = useState();
-
-  let fileSelectedHandler = (event) => {
-    setValue(URL.createObjectURL(event.target.files[0]));
-  };
-
   return (
     <div>
       <div className="profilepage-main-container">
         <div className="profilepage-popup-container">
           <h2 className="profilepage-header">Hi [name]! Welcome to Matchy!</h2>
           <p>please edit your profile to start finding your match!</p>
-
-          <img className="profile-photo-container" src={value} />
-          <input
-            type="file"
-            id="select-photo"
-            accept="image/*"
-            name="select-photo"
-            onChange={fileSelectedHandler}
-            hidden
-          />
-          <label className="btn-select-photo" for="select-photo" role="button">
-            select photo
-          </label>
-
+          <SelectPhoto />
           <div>
-            <label for="location-input"></label>
-
-            <input type="text" id="location-input" placeholder="City"></input>
+            <label for="city"></label>
+            <input type="text" id="city" placeholder="City"></input>
           </div>
-
           <div>
             <label for="hobbies"></label>
             <input
