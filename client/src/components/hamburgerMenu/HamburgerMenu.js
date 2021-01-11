50205644d0998f4b04a55b4c3cefb9e4b81d5bb5
danineubauer <danineubauerr@gmail.com>
2021-01-11 13:49:56 -0800

    edits menu popup

--- client/src/components/hamburgerMenu/HamburgerMenu.js
+++ client/src/components/hamburgerMenu/HamburgerMenu.js
@@ -2,21 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { HamburgerToggler } from "./hamburgerToggler/HamburgerToggler";
 import "./hamburgerMenu.css";
+import "./hamburgerToggler/HamburgerToggler.css";
 
 export const HamburgerMenu = () => {
   return (
     <div className="hamburger-menu">
-      <button className="btn-hamburger">
-        <HamburgerToggler />
-      </button>
-      <div>
-        <Link to="searchPage" className="hamburger-links">
+      <HamburgerToggler />
+      <div className="menu-popup">
+        <Link to="/searchpage" className="hamburger-links">
           Search matches
         </Link>
-        <Link to="profilePage" className="hamburger-links">
+        <Link to="/profilePage" className="hamburger-links">
           Profile page
         </Link>
-        <Link to="loginPage" className="hamburger-links">
+        <Link to="/loginPage" className="hamburger-links">
           Log in
         </Link>
       </div>
--- client/src/components/hamburgerMenu/hamburgerToggler/HamburgerToggler.js
+++ client/src/components/hamburgerMenu/hamburgerToggler/HamburgerToggler.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./HamburgerToggler.css";
 
 export const HamburgerToggler = () => {
@@ -8,6 +10,17 @@ export const HamburgerToggler = () => {
         <input className="hamburger-toggler" type="checkbox"></input>
         <div className="hamburger">
           <div></div>
+        </div>
+        <div className="menu-popup">
+          <Link to="/searchpage" className="hamburger-links">
+            Search matches
+          </Link>
+          <Link to="/profilePage" className="hamburger-links">
+            Profile page
+          </Link>
+          <Link to="/loginPage" className="hamburger-links">
+            Log in
+          </Link>
         </div>
       </div>
     </button>
