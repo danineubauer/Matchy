521983abc5f3afbc54bae0c801e331f8e35a3dda
Erez <luoruize@gmail.com>
2021-01-26 11:55:21 +0200

    search and fake data

--- client/src/components/searchPage/SearchPage.js
+++ client/src/components/searchPage/SearchPage.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import profilePicture from "../../assets/gal_gadot.jpg";
-import { callApi } from "../../mock/fakeServer";
+import { getUsers, searchUserByName } from "../../mock/fakeServer";
 import "./SearchPage.css";
 
 export const SearchPage = () => {
-  const onClickSearch = async () => { 
-    try {
-      const result = await callApi();
-      alert(result);
-    } catch (e) { 
-      alert('error!');
-    }
-  };
+  const [users, setUsers] = React.useState([]);
+  const [userIndex, setUserIndex] = React.useState(0);
+  const [searchString, setSearchString] = useState('');
+
+  React.useEffect(() => { 
+    (async () => { 
+      const users = await getUsers();
+      setUsers(users);
+    })()
+  }, [setUsers]);
+
+  React.useEffect(() => { 
+    (async () => { 
+      if (searchString.trim() !== '') {
+        const result = await searchUserByName(searchString);
+        console.log(result);
+      }
+    })();
+  }, [searchString]);
+
+  const user = users[userIndex];
 
   return (
     <div>
       <div className="searchpage-main-container">
         <div className="searchpage-popup-container">
           <h2 className="searchpage-header">Find your Matchy</h2>
+          <input placeholder="Search" onChange={(e) => {setSearchString(e.target.value)}} />
           <div className="searchpage-content-container">
             <div className="searchpage-photo-container">
               <img
@@ -29,17 +43,21 @@ export const SearchPage = () => {
               />
             </div>
 
-            <div className="searchpage-information-container">
-              <h2 className="searchpage-profile-name">Gal Gadot</h2>
-              <p>age:</p>
-              <p>location:</p>
-              <p>interested in:</p>
-              <p>hobbies:</p>
-            </div>
+            {
+              user && (
+                <div className="searchpage-information-container">
+                  <h2 className="searchpage-profile-name">{ `${user.firstName} ${user.lastName}` }</h2>
+                  <p>age: { user.age }</p>
+                  <p>location: {user.location}</p>
+                  <p>interested in: {user.interests}</p>
+                  <p>hobbies: {user.hobbies}</p>
+                </div>
+              )
+            }
           </div>
           <div className="nav-buttons-container">
             <button className="nav-button">❌</button>
-            <button className="nav-button" onClick={onClickSearch}>✅</button>
+            <button className="nav-button">✅</button>
           </div>
         </div>
       </div>
