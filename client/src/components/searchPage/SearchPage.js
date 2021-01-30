5f6a66af4dbc211de34a6bfc8de0753f593d50fd
danineubauer <danineubauerr@gmail.com>
2021-01-29 16:02:30 -0800

    add approved disaproved function

--- client/src/components/searchPage/SearchPage.js
+++ client/src/components/searchPage/SearchPage.js
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 import profilePicture from "../../assets/gal_gadot.jpg";
-import { getUsers, searchUserByName } from "../../mock/fakeServer";
+import { approveUser, getUsers, searchUserByName } from "../../mock/fakeServer";
 import "./SearchPage.css";
 
 export const SearchPage = () => {
   const [users, setUsers] = React.useState([]);
   const [userIndex, setUserIndex] = React.useState(0);
-  const [searchString, setSearchString] = useState('');
+  const [searchString, setSearchString] = useState("");
 
-  React.useEffect(() => { 
-    (async () => { 
+  React.useEffect(() => {
+    (async () => {
       const users = await getUsers();
       setUsers(users);
-    })()
+    })();
   }, [setUsers]);
 
-  React.useEffect(() => { 
-    (async () => { 
-      if (searchString.trim() !== '') {
+  React.useEffect(() => {
+    (async () => {
+      if (searchString.trim() !== "") {
         const result = await searchUserByName(searchString);
         console.log(result);
       }
@@ -31,7 +31,14 @@ export const SearchPage = () => {
       <div className="searchpage-main-container">
         <div className="searchpage-popup-container">
           <h2 className="searchpage-header">Find your Matchy</h2>
-          <input placeholder="Search" onChange={(e) => {setSearchString(e.target.value)}} />
+
+          <input
+            placeholder="Search"
+            onChange={(e) => {
+              setSearchString(e.target.value);
+            }}
+          />
+
           <div className="searchpage-content-container">
             <div className="searchpage-photo-container">
               <img
@@ -43,21 +50,30 @@ export const SearchPage = () => {
               />
             </div>
 
-            {
-              user && (
-                <div className="searchpage-information-container">
-                  <h2 className="searchpage-profile-name">{ `${user.firstName} ${user.lastName}` }</h2>
-                  <p>age: { user.age }</p>
-                  <p>location: {user.location}</p>
-                  <p>interested in: {user.interests}</p>
-                  <p>hobbies: {user.hobbies}</p>
-                </div>
-              )
-            }
+            {user && (
+              <div className="searchpage-information-container">
+                <h2 className="searchpage-profile-name">{`${user.firstName} ${user.lastName}`}</h2>
+                <p>age: {user.age}</p>
+                <p>location: {user.location}</p>
+                <p>interested in: {user.interests}</p>
+                <p>hobbies: {user.hobbies}</p>
+              </div>
+            )}
           </div>
           <div className="nav-buttons-container">
-            <button className="nav-button">❌</button>
-            <button className="nav-button">✅</button>
+            <button
+              className="nav-button"
+              onClick={() => setUserIndex(userIndex - 1)}
+            >
+              ❌
+            </button>
+            <button
+              className="nav-button"
+              user={user}
+              onClick={() => setUserIndex(userIndex + 1)}
+            >
+              ✅
+            </button>
           </div>
         </div>
       </div>
--- client/src/mock/fakeServer.js
+++ client/src/mock/fakeServer.js
@@ -1,4 +1,4 @@
-import Ape, {name, age, loremIpsum, arrayOf} from 'ape-mock';
+import Ape, { name, age, loremIpsum, arrayOf } from "ape-mock";
 
 const users = arrayOf({
   firstName: name().female(),
@@ -7,31 +7,33 @@ const users = arrayOf({
   location: loremIpsum().randomSequence().maxLength(2),
   hobbies: loremIpsum().randomSequence().maxLength(1),
   interests: loremIpsum().randomSequence().maxLength(2),
-}).repeat(50).generate();
+})
+  .repeat(50)
+  .generate();
 
 const rejected = [];
 const approved = [];
 
-export const callApi = (callback) => { 
-  return new Promise((resolve, reject) => { 
-    setTimeout(() => { 
+export const callApi = (callback) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
       resolve(callback());
     }, 1000);
   });
 };
 
-export const getUsers = () => { 
-  return new Promise(resolve => { 
-    setTimeout(() => { 
+export const getUsers = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
       resolve(users);
     }, 1000);
   });
 };
 
-export const searchUserByName = (searchString) => { 
-  return new Promise(resolve => { 
-    setTimeout(() => { 
-      const matches = users.filter(user => { 
+export const searchUserByName = (searchString) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const matches = users.filter((user) => {
         const fullName = `${user.firstName} ${user.lastName}`;
         return fullName.includes(searchString);
       });
@@ -41,6 +43,6 @@ export const searchUserByName = (searchString) => {
   });
 };
 
-export const approveUser = (user) => { };
+export const approveUser = (user) => {};
 
-export const rejectUser = (user) => { };
+export const rejectUser = (user) => {};
